9c237d477f22a06609ae31733827d958ba1bce02
Your Name <you@example.com>
2022-12-08 20:39:15 +0100

    cleaning yesterday code a little bit...not perfect but...

--- year-2022/day-7/no_space.py
+++ year-2022/day-7/no_space.py
@@ -9,49 +9,39 @@ with open('./year-2022/day-7/system.txt') as commandfile:
     for command in commands:
         com = command.strip().split(" ")
 
-        if com[1]=='cd' and com[2] == '/':
-            path=['start']
-            current='start'
-        if com[1]=='cd' and com[2]=='..':
-            if len(path) == 2:
-                path.pop()
+        if com[1] == 'cd':
+            if com[2] == '/':
+                path=['start']
                 current='start'
-            if len(path) == 1:
-                current='start'
-            else:
+            elif com[2]=='..':
                 path.pop()
                 current=path[-1]
-        
-        if com[1]=='cd' and com[2]!='..' and com[2]!='/':
-            current += com[2]
-            path.append(current)
-         
+            else:
+                current += com[2]
+                path.append(current)
 
         if com[0]=='dir':
             word = current+com[1]
             tree[word]=0
             folders.append(word)
         if com[0].isdigit():
-            tree[com[1]]=0
             for key in path:
                 tree[key] += int(com[0])
 
+values = []
 sum = 0
 for folder in folders:
+    values.append(tree[folder])
     if tree[folder] < 100000:
         sum += tree[folder]
-
-#second part
+        
 space_to_clean = tree['start'] - 40000000 
-
-sorted_tree = sorted(tree.items(), key=lambda x:x[1])
-
 minimum_folder_to_remove = 0
+ordered_values = sorted(values)
 
-for n in sorted_tree:
-    if tree[n[0]] > space_to_clean:
-        minimum_folder_to_remove += tree[n[0]]
+for n in ordered_values:
+    if n > space_to_clean:
+        minimum_folder_to_remove += n
         break
     
 print(sum, minimum_folder_to_remove)
- 
