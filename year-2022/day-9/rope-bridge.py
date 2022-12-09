09a72fa3c918778766dc84d547af8b468e91cebd
Your Name <you@example.com>
2022-12-09 23:00:34 +0100

    cleaning first part

--- year-2022/day-9/rope-bridge.py
+++ year-2022/day-9/rope-bridge.py
@@ -4,27 +4,25 @@ with open('./year-2022/day-9/steps.txt') as steps_file:
     positions = []
     tail = [0, 0]
     head = [0, 0]
-    pl = []
-
 
     for line in steps_line:
         
         steps = line.strip().split(' ')
         
-        #head_movements
         for step in range(0, int(steps[1])):
+
             if steps[0] == 'R':
                 head[0] += 1
                 #check tail and eventual movement
-                if head[0] - tail[0] > 1 or head[0] - tail[0] < -1:
+                if abs(head[0] - tail[0]) > 1:
                     tail[0] += 1
                     if head[1] != tail[1]:
                         tail[1] = head[1]
-
+                    
             if steps[0] == 'L':
                 head[0] -= 1
                 #check tail and eventual movement
-                if head[0] - tail[0] > 1 or head[0] - tail[0] < - 1 :
+                if abs(head[0] - tail[0]) > 1:
                     tail[0] -= 1
                     if head[1] != tail[1]:
                         tail[1] = head[1]
@@ -32,7 +30,7 @@ with open('./year-2022/day-9/steps.txt') as steps_file:
             if steps[0] == 'U':
                 head[1] += 1
                 #check tail and eventual movement
-                if head[1] - tail[1] > 1 or head[1] - tail[1] < -1:
+                if abs(head[1] - tail[1]) > 1:
                     tail[1] += 1
                     if head[0] != tail[0]:
                         tail[0] = head[0]
@@ -40,7 +38,7 @@ with open('./year-2022/day-9/steps.txt') as steps_file:
             if steps[0] == 'D':
                 head[1] -= 1
                 #check tail and eventual movement
-                if head[1] - tail[1] > 1 or head[1] - tail[1] < -1:
+                if abs(head[1] - tail[1]) > 1:
                     tail[1] -= 1
                     if head[0] != tail[0]:
                         tail[0] = head[0]
@@ -48,9 +46,7 @@ with open('./year-2022/day-9/steps.txt') as steps_file:
             nx = str(tail[0])
             ny = str(tail[1])
             np = nx+" "+ ny
-            if np not in pl:
-                pl.append(np)  
+            if np not in positions:
+                positions.append(np)  
 
-            
-   
-    print(len(pl))
+    print(len(positions))
